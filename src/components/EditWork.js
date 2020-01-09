cdd57373014a180ac7cf9db7420e67761859c926
Tomas Perers <9654120+zarkroc@users.noreply.github.com>
2020-01-09 15:10:18 +0100

    refactored

---
+++ src/components/EditWork.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import {
+    withFormik,
+    Form,
+    Field,
+    ErrorMessage
+} from 'formik';
+import * as yup from 'yup';
+
+const apiKey = process.env.REACT_APP_API_KEY;
+
+
+const MyFormik = ({
+    values,
+    errors,
+    touched,
+}) => (
+        <section>
+            <h2>Edit Work History</h2>
+            <Form>
+                <label htmlFor="company">Namn:<br />
+                    <Field id="company" type="text" name="company"
+                        className={errors.company && touched.company ? ' is-invalid' : ''}
+                        value={values.company} />
+                    <ErrorMessage component="span" className="error" name="company" />
+                </label><br />
+                <label htmlFor="desc">desc:<br />
+                    <Field id="desc" type="text" name="desc"
+                        className={errors.desc && touched.desc ? ' is-invalid' : ''}
+                        value={values.desc} />
+                    <ErrorMessage component="span" className="error" name="desc" />
+                </label><br />
+                <label htmlFor="start">start:<br />
+                    <Field id="start" type="text" name="start"
+                        className={errors.start && touched.start ? ' is-invalid' : ''}
+                        value={values.start} />
+                    <ErrorMessage component="span" className="error" name="start" />
+                </label><br />
+                <label htmlFor="stop">stop:<br />
+                    <Field id="stop" type="text" name="stop"
+                        className={errors.stop && touched.stop ? ' is-invalid' : ''}
+                        value={values.stop} />
+                    <ErrorMessage component="span" className="error" name="stop" />
+                </label><br />
+                <label htmlFor="role">role:<br />
+                    <Field id="role" type="text" name="role"
+                        className={errors.role && touched.role ? ' is-invalid' : ''}
+                        value={values.role} />
+                    <ErrorMessage component="span" className="error" name="role" />
+                </label><br />
+                <button className="btnPrimary">spara</button>
+            </Form>
+            {errors.apifault ? <p>{errors.apifault}</p> : null}
+        </section>
+    )
+
+
+const EditWork = withFormik({
+    enableReinitialize: true,
+    mapPropsToValues({ id, company, desc, role, start, stop }) {
+        return {
+            id: id || "",
+            company: company || "",
+            desc: desc || "",
+            role: role || "",
+            start: start || "",
+            stop: stop || ""
+        };
+    },
+
+    validationSchema: yup.object().shape({
+        company: yup.string().required("Antal är obligatoriskt"),
+        role: yup.string().required("Roll är obligatoriskt"),
+        desc: yup.string().required("Beskrivning är obligatoriskt"),
+        start: yup.string().required("Start är obligatoriskt"),
+        stop: yup.string().required("Stop är obligatoriskt"),
+    }),
+
+
+    handleSubmit: (values, { setSubmitting, resetForm, setStatus, setErrors, props }) => {
+        setTimeout(() => {
+
+            var data = {
+                id: values.id,
+                company: values.company,
+                desc: values.desc,
+                start: values.start,
+                stop: values.stop,
+                role: values.role
+            };
+            console.log('====================================');
+            console.log(data);
+            console.log('====================================');
+            var apiURL = "";
+
+            if (process.env.NODE_ENV === "production") {
+                apiURL = "https://api.perers.org/workhistory"
+            } else {
+                apiURL = "http://localhost:1337/workhistory"
+            }
+            fetch(apiURL, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'api_key': apiKey,
+                    'x-access-token': sessionStorage.getItem("token")
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(function (res) {
+                    if (res.errors) {
+                        setErrors({
+                            apifault: "Could not save reason: " + res.errors.detail
+                        })
+                    } else {
+                        resetForm();
+                        setSubmitting(false);
+                    }
+                });
+        }, 1000);
+    }
+})(MyFormik);
+
+export default EditWork;
+
--- src/components/History.js
+++ src/components/History.js
@@ -1,21 +1,26 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import EditHistory from './EditWork'
 
 const History = (props) => {
+    const [showEdit, setShowEdit] = useState(false);
+
+    const displayEdit = (() => {
+        setShowEdit(!showEdit);
+    });
+
     if (props.history) {
-        var output = props.history.map((element) => {
-            return (<section className="work" key={element._id}>
-                <p>{element.company}</p>
-                <p>{element.description}</p>
-                <p>{element.role}</p>
-                <p>{element.start} - {element.stop}</p>
-            </section>)
-        });
-        return (
-            <article className="history">
-                {output}
-            </article>
-        );
+            if(showEdit) {
+                return (<EditHistory id={props.history._id} company={props.history.company} desc={props.history.description} role={props.history.role} start={props.history.start} stop={props.history.stop} callback={displayEdit} />);
+            }
+            return (<section className="work" key={props.history._id}>
+                <p>{props.history.company}</p>
+                <p>{props.history.description}</p>
+                <p>{props.history.role}</p>
+                <p>{props.history.start} - {props.history.stop}</p>
+                {sessionStorage.getItem("token") ? <div>
+                    <button className="btnPrimary" onClick={displayEdit}>Edit</button>
+                    </div> : null}
+                </section>)
     }
     return (
         <section className="history">
